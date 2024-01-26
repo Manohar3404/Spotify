07e7fca879dead2495e50d400a4e30b149e3e632
Manohar <manoharachari3404@gmail.com>
2024-01-26 16:16:58 +0530

    javascript is optimized and title feature is added

---
+++ js
@@ -0,0 +1,12 @@
+const songs=[
+{songname:'Let me love u- Justin bieber',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+{songname:'Starboy - Weekend',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+{songname:'Bad Ass - Anirudh',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+{songname:'Choti choti party - DSP',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+{songname:'oo antava mama - DSP',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+{songname:'Sooride godugu patti - Ravibasur',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+{songname:'Dheera Dheera',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+{songname:'Let me love u- Justin bieber',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+{songname:'Starboy - Weekend',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+{songname:'Bad Ass - Anirudh',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+];
--- script.js
+++ script.js
@@ -1,140 +1,133 @@
 console.log('Welcome');
-let audio = new Audio('./songs/1.mp3');
+
+const audio = new Audio('./songs/1.mp3');
 const playbutton = document.querySelector('.playbutton1');
 const gitcontroller = document.getElementById('gif');
 const progressBar = document.querySelector('.bottom-range');
-const songItem=Array.from(document.querySelectorAll('.songItem'));
-let songidx=1;
+const songItems = Array.from(document.querySelectorAll('.songItem'));
+let songidx = 1;
 const backbutton = document.querySelector('.backbutton');
-const forwardbutton= document.querySelector('.forwardbutton');
-
+const forwardbutton = document.querySelector('.forwardbutton');
+const titleContainer = document.querySelector('.title'); // Add this line
 
 const songs=[
-{songname:'Let me love u- Justin bieber',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
-{songname:'Starboy - Weekend',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
-{songname:'Bad Ass - Anirudh',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
-{songname:'Choti choti party - DSP',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
-{songname:'oo antava mama - DSP',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
-{songname:'Sooride godugu patti - Ravibasur',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
-{songname:'Dheera Dheera',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
-{songname:'Let me love u- Justin bieber',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
-{songname:'Starboy - Weekend',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
-{songname:'Bad Ass - Anirudh',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
-];
+    {songname:'Let me love u- Justin bieber',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+    {songname:'Starboy - Weekend',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+    {songname:'Bad Ass - Anirudh',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+    {songname:'Choti choti party - DSP',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+    {songname:'oo antava mama - DSP',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+    {songname:'Sooride godugu patti - Ravibasur',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+    {songname:'Dheera Dheera',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+    {songname:'Let me love u- Justin bieber',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+    {songname:'Starboy - Weekend',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+    {songname:'Bad Ass - Anirudh',filepath:'./songs/1.mp3',coverpath:'./covers/1.jpg'},
+    ];
 
-songItem.forEach((element,i)=>{
-
-    element.getElementsByTagName('img')[0].src=`covers/${i+1}.jpg`;
-    // console.log( element.getElementsByTagName('img'));
-    element.getElementsByClassName('songTitle')[0].innerText=songs[i].songname;
-    // console.log( element.getElementsByClassName('songTitle')[0].innerText);
-    element.getElementsByClassName('playbutton')[0].id=`${i+1}`
-
+songItems.forEach((element, i) => {
+    element.querySelector('img').src = `covers/${i + 1}.jpg`;
+    element.querySelector('.songTitle').innerText = songs[i].songname;
+    element.querySelector('.playbutton').id = `${i + 1}`;
 });
-function makeallplay() {
-    Array.from(document.getElementsByClassName('playbutton')).forEach((element) => {
+
+function makeAllPlay() {
+    document.querySelectorAll('.playbutton').forEach((element) => {
         element.classList.remove('fa-pause');
         element.classList.add('fa-play');
     });
 }
 
+function songItemCheck(idx) {
+    document.querySelectorAll('.playbutton').forEach((element, i) => {
+        if (element.id == idx) {
+            element.classList.remove('fa-play');
+            element.classList.add('fa-pause');
+        }
+    });
+}
 
-Array.from(document.getElementsByClassName('playbutton')).forEach((element) => {
-    element.addEventListener('click',(el)=>{
-        // console.log(element);
-        if (el.target.classList.contains('fa-play')){
-        console.log(el.target);
-        makeallplay();
-        el.target.classList.remove('fa-play');
-        el.target.classList.add('fa-pause');
-        console.log('id',el.target.id);
-        audio.src=`./songs/${el.target.id}.mp3`;
-        songidx=el.target.id;
+function playPauseHandler(el) {
+    if (el.classList.contains('fa-play')) {
+        makeAllPlay();
+        el.classList.remove('fa-play');
+        el.classList.add('fa-pause');
+        audio.src = `./songs/${el.id}.mp3`;
+        songidx = el.id;
         audio.play();
         playbutton.classList.remove('fa-play');
         playbutton.classList.add('fa-pause');
         gitcontroller.style.opacity = 1;
-        }
-        else{
-            el.target.classList.remove('fa-pause');
-            el.target.classList.add('fa-play');
-            audio.pause();
-            playbutton.classList.remove('fa-pause');
-            playbutton.classList.add('fa-play');
-            gitcontroller.style.opacity = 0;
-        }
-    });
-});
-// songItemcheck(1);
- function songItemcheck(idx){
-    console.log('louda');
-    Array.from(document.getElementsByClassName('playbutton')).forEach((element,i)=>{
-        if (element.id==idx){
-            element.classList.remove('fa-play');
-            element.classList.add('fa-pause');
-            console.log('111111');
-        }
-    });
-
-
+        updateTitle(songs[songidx - 1].songname); // Add this line
+    } else {
+        el.classList.remove('fa-pause');
+        el.classList.add('fa-play');
+        audio.pause();
+        playbutton.classList.remove('fa-pause');
+        playbutton.classList.add('fa-play');
+        gitcontroller.style.opacity = 0;
+    }
 }
+
+document.querySelectorAll('.playbutton').forEach((element) => {
+    element.addEventListener('click', (el) => playPauseHandler(el.target));
+});
+
 playbutton.addEventListener('click', () => {
     if (audio.paused || audio.duration <= 0) {
         audio.play();
         playbutton.classList.remove('fa-play');
         playbutton.classList.add('fa-pause');
         gitcontroller.style.opacity = 1;
-        songItemcheck(songidx);
+        songItemCheck(songidx);
+        updateTitle(songs[songidx - 1].songname); // Add this line
     } else {
         audio.pause();
         playbutton.classList.remove('fa-pause');
         playbutton.classList.add('fa-play');
         gitcontroller.style.opacity = 0;
-        makeallplay();
+        makeAllPlay();
     }
 });
 
 audio.addEventListener('timeupdate', () => {
     const progress = parseInt((audio.currentTime * 100) / audio.duration);
-    // console.log(audio.currentTime);
-    // console.log(progress);
     progressBar.value = progress;
 });
-progressBar.addEventListener('change',()=>{
-    audio.currentTime=(progressBar.value*audio.duration/100);
+
+progressBar.addEventListener('change', () => {
+    audio.currentTime = (progressBar.value * audio.duration) / 100;
 });
-function check(idx){
-    if (playbutton.classList.contains('fa-play')){
+
+function check(idx) {
+    if (playbutton.classList.contains('fa-play')) {
         playbutton.classList.remove('fa-play');
         playbutton.classList.add('fa-pause');
         gitcontroller.style.opacity = 1;
-        
+        updateTitle(songs[idx - 1].songname); // Add this line
     }
-    songItemcheck(idx);
+    songItemCheck(idx);
 }
-backbutton.addEventListener('click',()=>{
-    if (songidx <= 1){
-        songidx=1;
+
+function updateTitle(title) {
+    titleContainer.innerText = title;
+}
+
+backbutton.addEventListener('click', () => {
+    if (songidx > 1) {
+        songidx -= 1;
+    } else {
+        songidx = songs.length;
     }
-    else{
-        songidx-=1;
-    }
-    console.log('back',songidx);
-    audio.src=`./songs/${songidx}.mp3`;
+    audio.src = `./songs/${songidx}.mp3`;
     audio.play();
-    makeallplay();
+    makeAllPlay();
     check(songidx);
 });
-forwardbutton.addEventListener('click',()=>{
-    songidx+=1;
 
-    if(songidx>10){
-        songidx=1;
-    }
-
-        
-    audio.src=`./songs/${songidx}.mp3`;
+forwardbutton.addEventListener('click', () => {
+    songidx = (songidx % songs.length) + 1;
+    audio.src = `./songs/${songidx}.mp3`;
     audio.play();
-    makeallplay();
+    makeAllPlay();
     check(songidx);
-})
+    updateTitle(songs[songidx - 1].songname); // Add this line
+});
